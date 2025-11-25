91fd687debedead59a2d16266f882d4bbfa1a1ee
Viderspace <viderspace@gmail.com>
2025-11-25 21:55:07 +0200

    Initial Commit

---
+++ QMKBridge/QMKBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QMKBridgeApp.swift
+//  QMKBridge
+//
+//  Created by Jonatan Vider on 25/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct QMKBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
